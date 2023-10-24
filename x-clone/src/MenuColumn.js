99cb9e6235c639bedb27360ce7db38faf1814fe8
sarabah <sarabah@remote.students.cs.ubc.ca>
2023-10-23 18:44:53 -0700

    creating the menu column

---
+++ x-clone/src/MenuColumn.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '.MenuColumn.css'
+
+function MenuColumn() {
+  return (
+    <div className="menucolumn">
+        {/* Twitter Icon */}
+        {/* Home */}
+        {/* Notifications */}
+        {/* Messages */}
+        {/* Lists */}
+        {/* Bookmarks */}
+        {/* Communities */}
+        {/* X Premium */}
+        {/* Profile */}
+        {/* More */}
+        {/* Post Button */}
+
+    </div>
+  )
+}
+
+export default MenuColumn
