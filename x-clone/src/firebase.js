bf4d61bebe71f00ec7e2d93fb4136c62d764868c
sarabah <sarabah@remote.students.cs.ubc.ca>
2023-10-29 16:07:10 -0700

    set up firebase

---
+++ x-clone/src/firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDuAPMgRohXbPCzGpIDQWIH7Y20cRewFKg",
+    authDomain: "x-clone-d9f9d.firebaseapp.com",
+    projectId: "x-clone-d9f9d",
+    storageBucket: "x-clone-d9f9d.appspot.com",
+    messagingSenderId: "356518960266",
+    appId: "1:356518960266:web:c29507eedac0e8d65ee173",
+    measurementId: "G-3WQJTJ12EC"
+  };
+
+  const firebaseApp = firebase.initialzieApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  
+  export default db;
