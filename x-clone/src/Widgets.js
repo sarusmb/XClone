03b06b7af2c5a17e8e1fb4d833cc1938772791da
sarabah <sarabah@remote.students.cs.ubc.ca>
2023-10-29 15:17:28 -0700

    added react twitter embed widgets

--- x-clone/src/Widgets.js
+++ x-clone/src/Widgets.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './Widgets.css'
+import SearchIcon from '@mui/icons-material/Search';
+import { 
+  TwitterTimelineEmbed,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      
+      <div className="widgets__widgetContainter">
+        <h2> What's Happening</h2>
+        <TwitterTweetEmbed tweetId={"1718446668583162215"}/>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="canucks"
+          options={{ height: 400 }}
+        />
+      </div>
+
+
     </div>
   )
 }
