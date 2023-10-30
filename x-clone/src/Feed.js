40d5a0aeaa1ca1378d9eefa3c7e4f4881898636d
sarabah <sarabah@remote.students.cs.ubc.ca>
2023-10-29 17:37:42 -0700

    pull tweets from firebase db and display on feed

--- x-clone/src/Feed.js
+++ x-clone/src/Feed.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Feed.css'
 import TweetBox from './TweetBox.js'
 import Tweet from './Tweet'
+import db from './firebase'
+import { collection, getDocs } from 'firebase/firestore/lite';
+
 
 function Feed() {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    getDocs(collection(db,'tweets')).then((snapshot) => (
+      setTweets(snapshot.docs.map(doc => doc.data()))
+    ))
+  }, [])
+
   return (
     <div className='feed'>
         <div className='feed__header'>
             <h2>Home</h2>
         </div>
-       <TweetBox/>
-       <Tweet/>
-    </div>
-            
-  )
+        <TweetBox/>
+          
+       {tweets.map(tweet => (
+        <Tweet 
+        displayName={ tweet.displayName }
+        userName={ tweet.userName }
+        avatar= { tweet.avatar }
+        verified= { tweet.verified}  
+        text= { tweet.text }  
+        image={ tweet.image }
+        />
+       ))}
+       
+    </div>     
+  );
 }
 
 export default Feed
--- x-clone/src/Tweet.js
+++ x-clone/src/Tweet.js
@@ -11,41 +11,37 @@ function Tweet({
   userName,
   avatar ,
   verified,
-  timestamp,
   text,
   image,
 }) {
   return (
     <div className="tweet">
       <div className="tweet__avatar">
-        <Avatar/>
+        <Avatar src={avatar}/>
       </div>
       <div className="tweet__body">
         <div className="tweet__header">
           <div className="tweet__headerText">
             <h3>
-              Joe Smith
+              {displayName}
               <span className="tweet__headerSpecial">
-                <VerifiedIcon className="tweet__verifiedIcon"/>
-                @JoeSmith
+                {verified && <VerifiedIcon className="tweet__verifiedIcon"/>}
+                {"  "}
+                @{userName}
                 </span>
             </h3>
           </div>
           <div className="tweet__headerDescription">
-            <p> Hello, this is Nyan Cat!</p>
+            <p>{text}</p>
           </div>
         </div>
-        <img src="https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1"/> 
+        <img src={image}/> 
         <div className="tweet__footer">
           <FavoriteBorderIcon fontSize="small" />
           <ChatBubbleOutlineIcon fontSize="small" />
         </div>
       </div>
-      
 
-      
-
-    
     </div>
   )
 }
