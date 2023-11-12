3b4d8473452c58ba3ffa99ee10bf5a1ee271fbf1
sarabah <sarabah@remote.students.cs.ubc.ca>
2023-11-12 01:12:38 -0800

    get data from db

--- x-clone/src/UserBadge.js
+++ x-clone/src/UserBadge.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './UserBadge.css'
 import { Avatar } from '@mui/material'
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import db from './firebase';
+import { doc, getDoc } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function UserBadge() {
+  const [userInfo, setUserInfo] = useState({});
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+   
+      const uid = user.uid;
+      // get the user document
+      const userRef = doc(db, "users", uid);
+      getDoc(userRef).then((doc) => {
+        setUserInfo(doc.data());
+      })
+    } 
+  });
+
   return (
     <div className='userBadge'>
-        <Avatar/>
+        <Avatar src={userInfo.profilePicture}/>
         <div className="userBadge__user">
-            <h3>Joe Smith</h3>
-            {" "} 
-            <span> @joeSmith </span>
+            <h3>{userInfo.firstName} {userInfo.lastName}</h3>
+            <span>@{userInfo.userName}</span>
         </div>
     </div>
   )
